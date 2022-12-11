9c03b250c45c1731cc9e87d4ece083cd6556b4bc
edlee <edlee@synapsoft.co.kr>
2022-12-12 07:20:31 +0900

    resize 방법 변경

--- js/main.js
+++ js/main.js
@@ -54,6 +54,15 @@ function selectToggle(){
     .on("mouseup", moveEnd);
 }
 
+function resizeOn() {
+    document.getElementById("canvas").setAttribute("class", "lowLayer");
+    document.getElementById("tmpCanvas").setAttribute("class", "highLayer");
+}
+
+function resizeOff() {
+    selectToggle()
+}
+
 
 /**
  * draw 관련 이벤트
@@ -177,13 +186,15 @@ function moveEnd() {
 /**
  * resize 이벤트
  */
-// path 자체를 수정하는 방법 시도
+// 1차 : path 자체를 수정하는 방법 시도
 // width, height를 이용해 svg를 resize해도 될 지는 따로 검토해볼 것. 스킨 어노테이션 표현을 봐야함
 
 // path 파싱 관련 글 : https://stackoverflow.com/questions/65211489/is-there-a-standard-for-parsing-svg-paths
 // svg1.1 standard인 Interface SVGPathSegment는 대부분 브라우저에서 미지원(https://svgwg.org/specs/paths/#InterfaceSVGPathSegment)
 // svg2에서 api가 제거되어서 앞으로도 사용될 일 없을듯
 // d3에도 d 파서는 없음
+
+// 2차 : 다시 그리는 방법 시도
 
 function mouseBorder() {
     var curObj = d3.select(this);
@@ -198,16 +209,13 @@ function mouseBorder() {
     d3Obj.on("mousedown", null);
     if(borderRight == curMouseX && borderBottom == curMouseY) {
         curObj.attr("cursor", "se-resize");
-        curObj.on("mousedown", resizeStart)
-        .on("mouseup", resizeEnd);
-    } else if(borderRight == curMouseX) {
+        curObj.on("mousedown", () => resizeReady(this, false,false))
+    } else if(borderRight == curMouseX || borderRight == curMouseX+1 || borderRight == curMouseX-1) {
         curObj.attr("cursor", "e-resize");
-        curObj.on("mousedown", resizeStart)
-        .on("mouseup", resizeEnd);
-    } else if(borderBottom == curMouseY) {
+        curObj.on("mousedown", () => resizeReady(this, false, true))
+    } else if(borderBottom == curMouseY || borderBottom == curMouseY+1 || borderBottom == curMouseY-1) {
         curObj.attr("cursor", "s-resize");
-        curObj.on("mousedown", resizeStart)
-        .on("mouseup", resizeEnd);
+        curObj.on("mousedown", () => resizeReady(this, true, false))
     } else {
         curObj.attr("cursor", "auto");
         curObj.on("mousedown", null);
@@ -215,56 +223,128 @@ function mouseBorder() {
     }
 }
 
-function resizeStart() {
-    const curMouse = d3.mouse(this);
-    storedMouseX = curMouse[0];
-    storedMouseY = curMouse[1];
-
-    d3.select(this)
-    .on("mousemove", resizeResize);
+function resizeReady(thisObj, xFix, yFix) {
+    resizeOn();
+    d3Obj = d3.select("#tmpCanvas");
+    d3Obj.on("mousemove", () => resizeStart(thisObj, xFix,yFix));
 }
 
+function resizeStart(thisObj, xFix, yFix) {
+    d3Obj.on("mousemove", null);
+    d3Obj.on("mousemove", resizeResize);
+    d3Obj.on("mouseup", resizeEnd);
 
-// 확대 하려면 이벤트 바인딩을 svg 밖에서 해야함
-// 확대과정에서 마우스 커서가 svg 밖으로 벗어나는것은 필연이기 때문
-function resizeResize() {
-    const curMouse = d3.mouse(this);
-    const xChange = curMouse[0] - storedMouseX;
-    const yChange = curMouse[1] - storedMouseY;
-    storedMouseX = curMouse[0];
-    storedMouseY = curMouse[1];
+    var curShape = document.getElementsByClassName("selected")[0];
+    var parentDiv = curShape.parentNode;
+    var m = d3.mouse(thisObj);
 
-    const svgObj = d3.select(this);
-    const svgWidth = parseInt(svgObj.style("width"),10);
-    const svgHeight = parseInt(svgObj.style("height"),10);
-
-    var curPath = document.getElementsByClassName("selected").item(0).lastElementChild;
-    curPath.setAttribute("class", "resizing");
-
-    var pathDriven = curPath.getAttribute("d");
-    const regex = /[^0-9.]/g;
-    pathDriven = pathDriven.replace(regex, " ");
-    const drivenArray = pathDriven.split(" ");
-
-    const x1 = parseInt(drivenArray[1], 10);
-    const y1 = parseInt(drivenArray[2], 10);
-    const x2 = parseInt(drivenArray[3], 10) + (xChange/svgWidth); // 각 x좌표 - (x좌표 전체합/마우스 이동) => path는 viewbox 기준이므로
-    const y2 = parseInt(drivenArray[4], 10) + (yChange/svgHeight);
-
+    // getBoundingClientRect()의 좌표는 상대좌표이다
+    // relative한 부모 요소의 좌표만큼 보정해줘야 함
+    // 참고 : https://mommoo.tistory.com/85
+    const startX = curShape.getBoundingClientRect().x - parentDiv.getBoundingClientRect().x;
+    const startY = curShape.getBoundingClientRect().y - parentDiv.getBoundingClientRect().y;
+    const fixX = xFix? startX + parseInt(curShape.getBoundingClientRect().width) : 0;
+    const fixY = yFix? startY + parseInt(curShape.getBoundingClientRect().height) : 0;
+    lineStart = {"x": startX, "y": startY};
     var lineData = [
-        {"x" : x1, "y" : y1},
-        {"x" : x2, "y" : y2}
+        lineStart,
+        {"x" : fixX == 0 ? m[0]:fixX, "y" : fixY == 0? m[1] : fixY}
     ];
 
-    // todo... svg 크기 조절하는 코드도 필요
-    d3.select(".resizing").attr("d", lineFunction(lineData));
+
+    curShape.parentNode.remove();
+    const tmpSvg = d3.create("svg");
+    tmpSvg.attr("width", 1280)
+    .attr("height", 720);
+    
+    draw = d3Obj.append(() => tmpSvg.node())
+        .append("path")
+        .attr("d", lineFunction(lineData))
+        .attr("style", pathStyle);
+
+    minMouse[0] = Math.min(minMouse[0], m[0]);
+    minMouse[1] = Math.min(minMouse[1], m[1]);
+    maxMouse[0] = Math.max(maxMouse[0], m[0]);
+    maxMouse[1] = Math.max(maxMouse[1], m[1]);
+    d3Obj.on("mousemove", () => resizeResize(thisObj, fixX, fixY));
 }
 
+function resizeResize(thisObj, fixX, fixY) {
+    var m = d3.mouse(thisObj);
+    var lineData = [
+        lineStart,
+        {"x" : fixX == 0 ? m[0]:fixX, "y" : fixY == 0? m[1] : fixY}
+    ];
+    draw.attr("d", lineFunction(lineData));
+
+    // pen 기준임. line이나 도형은 시작 끝만 비교해도 됨
+    minMouse[0] = Math.min(minMouse[0], m[0]);
+    minMouse[1] = Math.min(minMouse[1], m[1]);
+    maxMouse[0] = Math.max(maxMouse[0], m[0]);
+    maxMouse[1] = Math.max(maxMouse[1], m[1]);
+}
+
+
+// function resizeResize_backup() { // d를 직접 수정하는 방식 백업
+//     const curMouse = d3.mouse(this);
+//     const xChange = curMouse[0] - storedMouseX;
+//     const yChange = curMouse[1] - storedMouseY;
+//     storedMouseX = curMouse[0];
+//     storedMouseY = curMouse[1];
+
+//     const svgObj = d3.select(this);
+//     const svgWidth = parseInt(svgObj.style("width"),10);
+//     const svgHeight = parseInt(svgObj.style("height"),10);
+
+//     var curPath = document.getElementsByClassName("selected").item(0).lastElementChild;
+//     curPath.setAttribute("class", "resizing");
+
+//     var pathDriven = curPath.getAttribute("d");
+//     const regex = /[^0-9.]/g;
+//     pathDriven = pathDriven.replace(regex, " ");
+//     const drivenArray = pathDriven.split(" ");
+
+//     const x1 = parseInt(drivenArray[1], 10);
+//     const y1 = parseInt(drivenArray[2], 10);
+//     const x2 = parseInt(drivenArray[3], 10) + (xChange/svgWidth); // 각 x좌표 - (x좌표 전체합/마우스 이동) => path는 viewbox 기준이므로
+//     const y2 = parseInt(drivenArray[4], 10) + (yChange/svgHeight);
+
+//     var lineData = [
+//         {"x" : x1, "y" : y1},
+//         {"x" : x2, "y" : y2}
+//     ];
+
+//     // todo... svg 크기 조절하는 코드도 필요
+//     d3.select(".resizing").attr("d", lineFunction(lineData));
+// }
+
 function resizeEnd() {
-    d3.select(this)
-    .on("mousedown", moveStart)
-    .classed("resizing", false);
-    d3Obj.on("mousedown", moveStart);
+    d3Obj.on("mousemove", null);
+    const canvasElem = document.getElementById("canvas");
+
+    const drawDiv = document.createElement('div');
+    drawDiv.id = "draw_" + drawIdx;
+    drawDiv.style.position = "absolute";
+
+    const drawSvg = document.createElementNS(svgNS, 'svg');
+    const viewBoxVal = "" + minMouse[0] + " " + minMouse[1] + " " + (maxMouse[0] - minMouse[0]) + " " + (maxMouse[1] - minMouse[1]);
+    const drawSvgStyle = "left:" + minMouse[0] + "px; width: " + (maxMouse[0] - minMouse[0]) + "px; top: " + minMouse[1] + "px; height: " + (maxMouse[1] - minMouse[1]) + "px;" + pathStyle;
+    drawSvg.setAttribute("class", "scaledSvg");
+    drawSvg.setAttribute("viewBox", viewBoxVal);
+    drawSvg.style = drawSvgStyle;
+
+    const originSvg  = document.getElementById("tmpCanvas").lastElementChild;
+    const originDraw = originSvg.lastElementChild;
+    const copyDraw = originDraw.cloneNode(true);
+    originSvg.remove();
+
+    drawSvg.appendChild(copyDraw)
+    drawDiv.appendChild(drawSvg)
+    canvasElem.appendChild(drawDiv);
+
+    ++drawIdx;
+
+    resizeOff();
 }
 
 /**
